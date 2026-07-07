c3beade1ffb2a0241a51a5cd25ed3d949d94349c
Sai Karthik <sai14karthik@gmail.com>
2026-07-07 16:38:48 -0400

    Implement self attention v1-2

--- SelfAttention_v1.py
+++ SelfAttention_v1.py
@@ -5,7 +5,6 @@ class SelfAttention_v1(nn.Module):
     def __init__(self, d_in, d_out):
         super().__init__()
 
-        # Trainable weight matrices
         self.W_query = nn.Parameter(torch.rand(d_in, d_out))
         self.W_key = nn.Parameter(torch.rand(d_in, d_out))
         self.W_value = nn.Parameter(torch.rand(d_in, d_out))
