225d0eec63504401093f648815d7e6a0facf5f22
Sai Karthik <sai14karthik@gmail.com>
2026-07-10 14:28:33 -0400

    fix attention code v1 v2

--- SelfAttention_v1.py
+++ SelfAttention_v1.py
@@ -1,26 +1,30 @@
 import torch
 import torch.nn as nn
 
-class SelfAttention_v1(nn.Module):
-    def __init__(self, d_in, d_out):
+class SelfAttention_v2(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
         super().__init__()
 
-        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
-        self.W_key = nn.Parameter(torch.rand(d_in, d_out))
-        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
 
     def forward(self, x):
-        keys = x @ self.W_key
-        queries = x @ self.W_query
-        values = x @ self.W_value
+        # Compute Queries, Keys, and Values
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
 
+        # Compute attention scores
         attn_scores = queries @ keys.T
 
+        # Normalize scores
         attn_weights = torch.softmax(
             attn_scores / (keys.shape[-1] ** 0.5),
             dim=-1
         )
 
+        # Compute context vectors
         context_vec = attn_weights @ values
 
         return context_vec
